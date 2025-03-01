08d2a3bb02d4ce87f23d5a30dfb2c54698fd1aac
Narawit Sariddeechaikul <narawitpun@gmail.com>
2025-03-02 00:28:19 +0700

    add url to extracter interface

--- webcrawler/extracter.go
+++ webcrawler/extracter.go
@@ -4,7 +4,7 @@ import "book-search/webcrawler/models"
 
 type Extracter interface {
 	// IsValidBookPage checks if the html is a valid book page. Valid book pages are pages that contain book information.
-	IsValidBookPage(html string) bool
+	IsValidBookPage(url string, html string) bool
 
 	// ExtractBookInfo extracts the book information from the html and returns a Book struct.
 	Extract(html string) (*models.Book, error)
