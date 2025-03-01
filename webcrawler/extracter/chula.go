126f7a97257a06039791d81d82183f2b42965db3
peebaKU <baramee.254450@gmail.com>
2025-03-01 18:31:14 +0700

    edit name func

--- webcrawler/extracter/chula.go
+++ webcrawler/extracter/chula.go
@@ -1,11 +1,15 @@
 package extracter
 
-import (
-	"book-search/webcrawler/models"
-)
+import "book-search/webcrawler/models"
 
 type ChulaExtracter struct{}
 
-func ChulaExtracter(html string) {
-	fmt.Println("Hello")
+func (n ChulaExtracter) IsValidBookPage(html string) bool {
+	// Implement logic to check if the HTML is a valid Naiin book page
+	return false
+}
+
+func (n ChulaExtracter) Extract(html string) (*models.Book, error) {
+	// Implement logic to extract book information from Naiin HTML
+	return &models.Book{}, nil
 }
